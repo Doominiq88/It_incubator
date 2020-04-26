32f32fb01038fffd1b807782c22ee5dba104806d
Ivan P <88talem@gmail.com>
2020-04-26 22:06:26 +0300

    feat: add moldules

---
+++ array.js
@@ -0,0 +1,36 @@
+ const people = [{
+
+         name: 'Petya',
+         age: 22,
+         description: 'fdvdfvdvkjdfkvfdjvb'
+
+     },
+     {
+         name: 'Vasya',
+         age: 24,
+         description: 'fdvdfvdvkjdfkvfdjvb'
+     },
+     {
+         name: 'Ivan',
+         age: 28,
+         description: 'fdvdfvdvkjdfkvfdjvb'
+     },
+     {
+         name: 'Pasha',
+         age: 26,
+         description: 'fdvdfvdvkjdfkvfdjvb'
+     },
+     {
+         name: 'Petya',
+         age: 22,
+         description: 'fdvdfvdvkjdfkvfdjvb'
+     },
+     {
+         name: 'Petya',
+         age: 22,
+         description: 'fdvdfvdvkjdfkvfdjvb'
+     },
+ ]
+ console.log(people)
+
+ export default people;
--- script.js
+++ script.js
@@ -1,121 +1,120 @@
-const team = document.querySelector('.team__items');
-const drop = document.querySelector('.team__drop');
-const teamImg = document.querySelectorAll('.team__img');
-const teamFoto = document.querySelectorAll('.team__foto');
+  import people from './array.js';
+
+  // console.log(people);
+
+  const team = document.querySelector('.team__items');
+  const drop = document.querySelector('.team__drop');
+  const teamImg = document.querySelectorAll('.team__img');
+  const teamFoto = document.querySelectorAll('.team__foto');
 
 
-//create pop-up Img
+  //create pop-up Img
 
-const popUp = document.querySelector('.about__blocks__img');
-const popUpDiv = document.createElement('div');
-popUpDiv.classList.add('pop-up')
-popUp.append(popUpDiv);
+  const popUp = document.querySelector('.about__blocks__img');
+  const popUpDiv = document.createElement('div');
+  popUpDiv.classList.add('pop-up')
+  popUp.append(popUpDiv);
 
-const buttonClose = document.createElement('button');
-buttonClose.classList.add('close-btn');
-popUpDiv.append(buttonClose);
+  const buttonClose = document.createElement('button');
+  buttonClose.classList.add('close-btn');
+  popUpDiv.append(buttonClose);
 
-buttonClose.innerHTML = 'X';
+  buttonClose.innerHTML = 'X';
 
+  const site = document.querySelector('.site');
+  const secondDiv = document.createElement('div');
+  secondDiv.classList.add('some-div');
+  site.append(secondDiv);
 
-
-const site = document.querySelector('.site');
-const secondDiv = document.createElement('div');
-secondDiv.classList.add('some-div')
-site.append(secondDiv);
-
-const workImages = document.querySelectorAll('.works__items img');
-const workItem = document.querySelector('.works__items');
-console.log(workItem);
-
-const imgDiv = document.createElement('div');
-imgDiv.classList.add('img-div');
-popUpDiv.append(buttonClose);
+  const workImages = document.querySelectorAll('.works__items img');
+  const workItem = document.querySelector('.works__items');
 
 
-const buttonCloseImg = document.createElement('button');
-buttonCloseImg.classList.add('img-close-btn');
-imgDiv.append(buttonCloseImg);
+  const imgDiv = document.createElement('div');
+  imgDiv.classList.add('img-div');
+  popUpDiv.append(buttonClose);
 
-buttonCloseImg.innerHTML = 'X';
+  const buttonCloseImg = document.createElement('button');
+  buttonCloseImg.classList.add('img-close-btn');
+  imgDiv.append(buttonCloseImg);
+
+  buttonCloseImg.innerHTML = 'X';
 
 
-workImages.forEach((element) => {
-    element.addEventListener('click', (event) => {
-        let alt = event.target.getAttribute('alt')
-        workItem.append(imgDiv);
-        imgDiv.classList.add('display-on');
-        secondDiv.classList.add('display-on');
+  workImages.forEach((element) => {
+      element.addEventListener('click', (event) => {
+          let alt = event.target.getAttribute('alt')
+          workItem.append(imgDiv);
+          imgDiv.classList.add('display-on');
+          secondDiv.classList.add('display-on');
 
-        switch (alt) {
-            case 'pic1':
-                imgDiv.classList.add('background-icecream');
-                break;
-            case 'pic2':
-                imgDiv.classList.add('background-visit');
-                break;
-            case 'pic3':
-                imgDiv.classList.add('background-bottle');
-                break;
-            case 'pic4':
-                imgDiv.classList.add('background-book');
-                break;
-            case 'pic5':
-                imgDiv.classList.add('background-euro');
-                break;
-            case 'pic6':
-                imgDiv.classList.add('background-pack');
-                break;
-        }
+          switch (alt) {
+              case 'pic1':
+                  imgDiv.classList.add('background-icecream');
+                  break;
+              case 'pic2':
+                  imgDiv.classList.add('background-visit');
+                  break;
+              case 'pic3':
+                  imgDiv.classList.add('background-bottle');
+                  break;
+              case 'pic4':
+                  imgDiv.classList.add('background-book');
+                  break;
+              case 'pic5':
+                  imgDiv.classList.add('background-euro');
+                  break;
+              case 'pic6':
+                  imgDiv.classList.add('background-pack');
+                  break;
+          }
+      });
+  });
 
-    });
+  popUp.addEventListener('click', (event) => {
+      if (event.target.tagName === 'BUTTON') return;
+      popUpDiv.classList.add('display-on');
+      popUpDiv.classList.add('background-chair');
+      secondDiv.classList.add('display-on');
 
-});
+  });
 
-popUp.addEventListener('click', (event) => {
-    if (event.target.tagName === 'BUTTON') return;
-    popUpDiv.classList.add('display-on');
-    popUpDiv.classList.add('background-chair');
-    secondDiv.classList.add('display-on');
+  imgDiv.addEventListener('click', (event) => {
+      if (event.target.tagName === 'BUTTON') return;
+      secondDiv.classList.add('display-on');
 
-});
+  })
 
-imgDiv.addEventListener('click', (event) => {
-    if (event.target.tagName === 'BUTTON') return;
-    secondDiv.classList.add('display-on');
+  buttonClose.addEventListener('click', () => {
+      popUpDiv.classList.remove('display-on');
+      secondDiv.classList.remove('display-on');
+      popUpDiv.classList.remove('background-chair');
+      popUpDiv.classList.remove('background-chair');
 
-})
+  });
 
-buttonClose.addEventListener('click', () => {
-    popUpDiv.classList.remove('display-on');
-    secondDiv.classList.remove('display-on');
-    popUpDiv.classList.remove('background-chair');
-    popUpDiv.classList.remove('background-chair');
+  buttonCloseImg.addEventListener('click', () => {
+      imgDiv.classList.remove('display-on');
+      secondDiv.classList.remove('display-on');
+      imgDiv.classList.remove('background-icecream');
+      imgDiv.classList.remove('background-visit');
+      imgDiv.classList.remove('background-bottle');
+      imgDiv.classList.remove('background-book');
+      imgDiv.classList.remove('background-euro');
+      imgDiv.classList.remove('background-pack');
+  });
 
-});
+  // hover
 
-buttonCloseImg.addEventListener('click', () => {
-    imgDiv.classList.remove('display-on');
-    secondDiv.classList.remove('display-on');
-    imgDiv.classList.remove('background-icecream');
-    imgDiv.classList.remove('background-visit');
-    imgDiv.classList.remove('background-bottle');
-    imgDiv.classList.remove('background-book');
-    imgDiv.classList.remove('background-euro');
-    imgDiv.classList.remove('background-pack');
-});
+  teamFoto.forEach((element) => {
+      element.addEventListener('mouseover', () => {
+          element.children[0].classList.add('drop_on');
+          console.log(element.children[0]);
+      });
+  });
 
-// hover
-
-teamImg.forEach((element) => {
-    element.addEventListener('mouseover', (event) => {
-        event.target.previousElementSibling.classList.add('drop_on');
-    });
-})
-
-teamImg.forEach((element) => {
-    element.addEventListener('mouseout', (event) => {
-        if (event.target.tagName != 'IMG') return;
-        event.target.previousElementSibling.classList.remove('drop_on');
-    });
-})
+  teamFoto.forEach((element) => {
+      element.addEventListener('mouseout', () => {
+          element.children[0].classList.remove('drop_on');
+      });
+  });
